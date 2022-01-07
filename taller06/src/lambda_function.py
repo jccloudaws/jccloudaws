69de2dfe0253cf8f425ab14d2649a5537f9fa3d6
EC2 Default User <ec2-user@ip-172-31-37-238.ec2.internal>
2022-01-07 22:15:30 +0000

    talleres

--- taller06/src/lambda_function.py
+++ taller06/src/lambda_function.py
@@ -1,27 +1,15 @@
 import json
 import requests
 import boto3
-from boto3.dynamodb.conditions import Key, Attr
-import urllib.parse
 import os
+from common.secretmanager import SecretManager
 
-secret_client = boto3.client('secretsmanager')
-dynamodb_resource = boto3.resource('dynamodb')
-    
+sm = SecretManager()
+
 def lambda_handler(event, context):
     print("event",event)
-    secretId= os.getenv("credentialsEmblue")
-    secret_response = secret_client.get_secret_value(SecretId=secretId)
-    secret = json.loads(secret_response.get('SecretString'))
+    secret=sm.get_secret()
     print("secret",secret)
-    #---------------------------------------------------
-    tableConfig= os.getenv("table")
-    marca=event.get("marca",None)
-    table = dynamodb_resource.Table(tableConfig)
-    response = table.query(
-                KeyConditionExpression=Key("marca").eq(marca)
-            )
-    print (response['Items'])
     
 if __name__ == "__main__":
     event={"key1":"value1"}
---
+++ taller07/deploy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+ENV=$1
+
+S3_BUCKET="rpa-cfn-artifacts-40888866-us-east-1"
+
+aws s3 mb s3://rpa-cfn-artifacts-40888866-us-east-1
+
+STACK_NAME="rpa-capacitacion-cloud-aws-taller-lambda-07"
+REGION="us-east-1"
+DIRNAME=$(dirname "$0")
+PREFIX=RPA-TALLER-LAMBDA-07
+#pip install -r requirements.txt -t ./src
+#pip  install -q -r requirements.txt  -t src/python
+
+mkdir ./output
+aws cloudformation package \
+ --template-file $DIRNAME/template.yaml \
+ --s3-bucket ${S3_BUCKET} \
+ --s3-prefix $PREFIX \
+ --output-template-file $DIRNAME/output/packaged-template.yaml
+
+out=$(aws cloudformation describe-stacks --region $REGION --stack-name $STACK_NAME)
+echo $out
+if [ -z "$out" ]; then
+   echo -e "Stack not exist, creating ...!!"
+   aws cloudformation create-stack \
+        --stack-name ${STACK_NAME} \
+        --parameters file://parameters-${ENV}.json \
+        --template-body file://$DIRNAME/output/packaged-template.yaml \
+        --tags file://tags-${ENV}.json \
+        --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
+        
+   echo "Waiting for stack to be created ..."
+   aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME};
+else
+   echo -e "Stack exists, attempting update ..."
+   set +e
+   aws cloudformation update-stack \
+      --stack-name $STACK_NAME \
+      --parameters file://$DIRNAME/parameters-${ENV}.json \
+      --template-body file://$DIRNAME/output/packaged-template.yaml \
+      --tags file://$DIRNAME/tags-${ENV}.json \
+      --capabilities "CAPABILITY_NAMED_IAM" "CAPABILITY_AUTO_EXPAND"
+      
+   set -e
+   echo "Waiting for stack update to complete ..."
+   aws cloudformation wait stack-update-complete --stack-name ${STACK_NAME};
+   echo "Finished create/update successfully!"
+fi
+exit 0;
