04ae17b58f1e5bc122f63b73928c34873657cfea
Mary Major <mary.major@example.com>
2022-01-25 00:30:13 +0000

    taller13

---
+++ xx/README.md
@@ -0,0 +1,26 @@
+- 👋 Hi, I’m @jccloudaws
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+jccloudaws/jccloudaws is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+**Taller01:**
+- Ingresar a cloud9 
+- En la terminal ejecutar : ssh-keygen
+- lanzar el siguiente comando : git clone git@github.com:jccloudaws/jccloudaws.git
+- cd jccloudaws/taller01
+- editar el archivo deploy.sh (linea 5 , linea 7 , cambiar {tu-dni} por el numero de documento)
+- chmod +x ./deploy.sh
+- ./deploy.sh dev
+- pip install -r src/requirements.txt
+- python src/lambda_function.py
+- 
+
+set AWS_CLI_FILE_ENCODING=UTF-8
+
+aws dynamodb batch-write-item --request-items file://table-legales.json
+aws dynamodb batch-write-item --request-items file://UAT-RTD-DenegacionSobregiro-Template.json
---
+++ xx/src/lambda-financial/lambda_function.py
@@ -0,0 +1,33 @@
+import json
+import os
+import boto3
+
+
+#secret_client = boto3.client('secretsmanager')
+dynamodb_resource = boto3.resource('dynamodb')
+def lambda_handler(event, context):
+    table_financial= os.getenv("table")
+    print("metodo",event['httpMethod'])
+    if event['httpMethod']!= 'POST':
+        return {"statusCode": 200,"headers": {"Content-Type": "application/json"},
+                    "body": json.dumps({'status': False,
+                    'code_status': 400, 'menssage': "http Method not support",
+                    'data': {}})}
+    if event['path'] in ['/rpa/financial-details']:
+        try:
+            playload = json.loads(event['body'])
+            table = dynamodb_resource.Table(table_financial)
+            row={"ndoc":playload["ndoc"],"tdoc":playload["tdoc"],"saldo":playload["saldo"]}
+            table.put_item(Item= row)
+            return {"statusCode": 200,"headers": {"Content-Type": "application/json"},
+                "body": json.dumps(
+                    {'status': True,
+                    'code_status': 200,
+                    'menssage': "Registro creado correctamente - financial-details",
+                    'data': row})}
+        except Exception as error:
+            print(error)
+            return {"statusCode": 200,"headers": {"Content-Type": "application/json"},
+                    "body": json.dumps({'status': False,
+                    'code_status': 400, 'menssage': "financial-details",
+                    'data': error.__str__()})}
